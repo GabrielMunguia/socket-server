1bc28068e43164d163a2f1c493605e862da7888f
GabrielMunguia <juayua2011@gmail.com>
2022-01-09 03:19:18 -0600

    solucion bug

--- models/Server.js
+++ models/Server.js
@@ -1,61 +1,59 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
 
+const { socketController } = require('../sockets/controller');
+
 class Server {
-  constructor() {
-    this.app = express();
-    this.port = process.env.PORT;
- 
-    this.server = require('http').createServer(this.app);
-    this.io = require('socket.io')(this.server);
-   
-    this.paths={
-   
+
+    constructor() {
+        this.app    = express();
+        this.port   = process.env.PORT;
+        this.server = require('http').createServer( this.app );
+        this.io     = require('socket.io')( this.server );
+
+        this.paths = {};
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+
+        // Sockets
+        this.sockets();
     }
-   
 
+    middlewares() {
 
+        // CORS
+        this.app.use( cors() );
 
-     //MIDDLEWARES
-     this.middleware();
-    //RUTAS
-    this.routes();
+        // Directorio Público
+        this.app.use( express.static('public') );
 
-    //socket
+    }
 
-    this.socket();
-  }
+    routes() {
+        
+        // this.app.use( this.paths.auth, require('../routes/auth'));
+        
+    }
 
-  routes() {
+    sockets() {
 
-   
-    
-  }
+        this.io.on('connection', socketController );
 
-  middleware() {
-      //Directorio publico
-      this.app.use(express.static('public'))
-      //CORS
-      this.app.use(cors())
-      //Lectura y parseo del body en JSON
-      this.app.use(express.json())
+    }
 
+    listen() {
+        this.server.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
 
-  }
-
-  async connectarDB(){
-    await dbConnection()
-  }
-
-  listen() {
-    this.server.listen(process.env.PORT, () => {
-      console.log("localhost:", process.env.PORT);
-    });
-  }
-  socket(){
-    
-    this.io.on("connection", socketController)
-  }
 }
+
+
+
 
 module.exports = Server;
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,17 +1,27 @@
 
-const socketController=(socket) => {
+
+
+const socketController = (socket) => {
+    
     console.log('Cliente conectado', socket.id );
 
     socket.on('disconnect', () => {
         console.log('Cliente desconectado', socket.id );
     });
 
-    socket.on('enviar-mensaje',(payload,callback)=>{
-        socket.broadcast.emit('enviar-mensaje',payload)
-      callback({msj:'todo salio bien',id:'123456'})
+    socket.on('enviar-mensaje', ( payload, callback ) => {
+        
+        const id = 123456789;
+        callback( id );
+
+        socket.broadcast.emit('enviar-mensaje', payload );
+
     })
 
+}
 
-  } 
 
-  moodule.export =socketController
+
+module.exports = {
+    socketController
+}
